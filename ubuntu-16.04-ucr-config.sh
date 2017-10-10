61987545a489eb9b9ccdf661621149de1196ef7e
Xiomara <xiomara@Xiomy>
2017-10-10 16:18:30 -0600

    Modificación del instalador del complemento

--- ubuntu-16.04-ucr-config.sh
+++ ubuntu-16.04-ucr-config.sh
@@ -113,8 +113,7 @@ packages="$packages libreoffice libreoffice-l10n-en-za libreoffice-l10n-en-gb li
 # Plantillas
 # Agrega mas diferentes tipos de plantillas para presentaciones, hojas de calculo, entre otras.
 # ultima version en https://extensions.openoffice.org/en/project/SunTemplatepack_1_es
-wget https://sourceforge.net/projects/aoo-extensions/files/301/1/sun_odf_template_pack_es.oxt/download
-mv download sun_odf_template_pack_es.oxt
+wget -O sun_odf_template_pack_es.oxt https://sourceforge.net/projects/aoo-extensions/files/301/1/sun_odf_template_pack_es.oxt/download
 sudo unopkg add --shared sun_odf_template_pack_es.oxt
 rm sun_odf_template_pack_es.oxt
 

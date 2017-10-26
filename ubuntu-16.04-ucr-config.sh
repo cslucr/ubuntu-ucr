22359351d3c4c7d38e1aaa852de0e5263f581139
Andrea Torres <amekare@gmail.com>
2017-10-26 16:15:58 -0600

    se agregaron paquetes innecesarios para eliminar antes de la instalacion final, para no descargarlos sin necesidad

--- ubuntu-16.04-ucr-config.sh
+++ ubuntu-16.04-ucr-config.sh
@@ -294,7 +294,16 @@ if grep -q "Unity" /usr/share/xsessions/*;        then packages="$packages unity
 if grep -q "gnome-shell" /usr/share/xsessions/*;  then packages="$packages gnome-tweak-tool"; fi
 if grep -q "MATE" /usr/share/xsessions/*;         then packages="$packages mate-tweak"; fi
 
-purgepackages="$purgepackages evolution evolution-plugins evolution-common libevolution evolution-data-server-online-accounts tilda"
+purgepackages="$purgepackages evolution evolution-plugins evolution-common libevolution evolution-data-server-online-accounts tilda
+libreoffice-help-fr libreoffice-help-it libreoffice-help-pt libreoffice-help-pt-br libreoffice-help-ru
+libreoffice-help-zh-cn libreoffice-help-zh-tw libreoffice-l10n-de libreoffice-l10n-en-gb
+libreoffice-l10n-en-za libreoffice-l10n-fr libreoffice-l10n-it libreoffice-l10n-pt libreoffice-l10n-pt-br
+libreoffice-l10n-ru libreoffice-l10n-zh-cn libreoffice-l10n-zh-tw
+firefox-locale-de firefox-locale-fr firefox-locale-it firefox-locale-pt firefox-locale-ru firefox-locale-zh-hans
+thunderbird-locale-de thunderbird-locale-en-gb thunderbird-locale-es-ar thunderbird-locale-es-es thunderbird-locale-fr 
+thunderbird-locale-it thunderbird-locale-pt thunderbird-locale-pt-br thunderbird-locale-pt-pt thunderbird-locale-ru 
+thunderbird-locale-zh-cn thunderbird-locale-zh-hans thunderbird-locale-zh-hant thunderbird-locale-zh-tw"
+
 autostart="$autostart /usr/share/applications/caffeine.desktop /usr/share/applications/caffeine-indicator.desktop"
 
 # Actualizacion del sistema e instalacion de los paquetes indicados

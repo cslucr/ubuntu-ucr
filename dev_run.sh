81f4269408a238ddc26e78a1162f9bf315060610
Leo Jiménez <leojimenezcr@gmail.com>
2018-07-19 13:58:25 -0600

    Elminados elementos innecesarios o que tienen que ser sustituidos

--- dev_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p cache/apt
-mkdir -p cache/wget
-
-echo "Removing temporary file"
-sudo rm -rf ubuntu-iso-customization
-echo "Creating ISO"
-./ubuntu-iso-customization.sh -d -c "cache/apt" -w "cache/wget" ubuntu-mate-16.04.3-desktop-amd64.iso

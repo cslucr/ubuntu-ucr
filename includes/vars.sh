45a0b900f3af6fd35a7e43de95f6245666a90c11
constrict0r <camattei@gmail.com>
2018-10-08 10:05:27 -0600

    Move all variable to include/vars.sh

--- includes/vars.sh
+++ includes/vars.sh
@@ -19,5 +19,18 @@ SCRIPTPATH=$(readlink -f $0)
 BASEDIR=$(dirname "$SCRIPTPATH")
 
 # Computer architecture.
-ARCH=$(uname -m)
+arch=$(uname -m)
+
+# This variable aggregates package names that will be installed:
+# packages="$packages package1 package2 package3"
+packages=""
+
+# This variable aggregates package names that will be uninstalled
+#  purgepackages="$purgepackages package1 package2 package3"
+purgepackages=""
+
+# This variable aggregates path to .desktop files for applications
+# that must run on startup:
+#  autostart="$autostart path1 path2 path3"
+autostart=""
 

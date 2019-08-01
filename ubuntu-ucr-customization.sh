4303b171a79ed791caed9776691a2ba58b39e6b6
Leo Jiménez <leojimenezcr@gmail.com>
2019-08-01 16:42:36 -0600

    Repositorio de Dropbox

--- ubuntu-ucr-customization.sh
+++ ubuntu-ucr-customization.sh
@@ -196,7 +196,10 @@ packages="$packages shotwell"
 #
 # Añade el repositorio de dropbox, pero no instala el paquete. Si no que
 # lo deja disponible para cuando un usuario requiera utilizarlo.
-# TODO: El paquete existe pero no el repositorio.
+sudo sh -c 'echo "deb [arch=i386,amd64] http://linux.dropbox.com/ubuntu bionic main" > /etc/apt/sources.list.d/dropbox.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E || error_exit "Error al agregar llave para repositorio dropbox"
+
+#packages="$packages dropbox"
 
 # GIMP
 #

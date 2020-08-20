18bde644cd6c41c7e58c130727d4bcdcce7a0ba0
Leo Jiménez <leojimenezcr@gmail.com>
2020-08-19 19:34:34 -0600

    Corrección nombre de paquete linux-firmware

--- ubuntu-ucr-customization.sh
+++ ubuntu-ucr-customization.sh
@@ -190,7 +190,7 @@ packages="$packages flatpak"
 # - Drivers comunes para algunas tarjetas de red.
 # - Arc-theme, popular tema flat.
 # - Yaru theme (iconos, cursores, sonidos).
-packages="$packages unattended-upgrades caffeine vlc shotwell rar p7zip-rar exfat-fuse exfat-utils hfsplus hfsutils ntfs-3g inux-firmware firmware-b43-installer arc-theme yaru-theme-icon yaru-theme-sound"
+packages="$packages unattended-upgrades caffeine vlc shotwell rar p7zip-rar exfat-fuse exfat-utils hfsplus hfsutils ntfs-3g linux-firmware firmware-b43-installer arc-theme yaru-theme-icon yaru-theme-sound"
 # - configuracion avanzada para reestablecer tema predeterminado o ajustes adicionales
   if grep -q "gnome-shell" /usr/share/xsessions/*;  then packages="$packages gnome-tweak-tool"; fi
   # if grep -q "MATE" /usr/share/xsessions/*;         then packages="$packages mate-tweak"; fi
